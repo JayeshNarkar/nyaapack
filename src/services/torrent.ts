5faff129c74d0b5ea789c19c22f578ef19731329
JayeshNarkar <jayesh.narkar18@gmail.com>
2025-09-16 15:55:33 +0530

    error testing

--- src/services/torrent.ts
+++ src/services/torrent.ts
@@ -2,36 +2,47 @@ import { createDownloadSessionDB } from "../db/downloadSession.js";
 import { addTorrentToDB } from "../db/torrent.js";
 import { downloadDir } from "../index.js";
 import { StatusResponse, TorrentSchema } from "../utils/types.js";
+import WebTorrent from "webtorrent";
+
+const client = new WebTorrent();
 
 function startDownload(torrent: TorrentSchema | undefined): StatusResponse {
-  if (!torrent) {
+  try {
+    if (!torrent) {
+      return {
+        status: 500,
+        message: "Torrent object cannot be empty",
+      };
+    }
+    const torrentID = addTorrentToDB(torrent);
+
+    if (!torrentID) {
+      return {
+        status: 500,
+        message: "Error in Creating torrent entry",
+      };
+    }
+
+    const downloadSessionID = createDownloadSessionDB(torrent.id, downloadDir);
+
+    if (!downloadSessionID) {
+      return {
+        status: 500,
+        message: "Error in Creating download session entry",
+      };
+    }
+
+    return {
+      status: 200,
+      message: "Torrent has started downloading successfully!",
+    };
+  } catch (error) {
+    console.log(error);
     return {
       status: 500,
-      message: "Torrent object cannot be empty",
+      message: "Internal Error!",
     };
   }
-  const torrentID = addTorrentToDB(torrent);
-
-  if (!torrentID) {
-    return {
-      status: 500,
-      message: "Error in Creating torrent entry",
-    };
-  }
-
-  const downloadSessionID = createDownloadSessionDB(torrent.id, downloadDir);
-
-  if (!downloadSessionID) {
-    return {
-      status: 500,
-      message: "Error in Creating download session entry",
-    };
-  }
-
-  return {
-    status: 200,
-    message: "Torrent has started downloading successfully!",
-  };
 }
 
 export { startDownload };
